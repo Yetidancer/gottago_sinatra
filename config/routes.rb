f13c309ea75e26132d55431fdbc2492f232f8eff
Ezekiel Clark <clarkzeke@gmail.com>
2020-04-13 15:18:08 -0600

    add config to sinatra

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
-require 'sinatra'
-require 'faraday'
-require 'pry'
-require_relative 'restrooms_service'
-
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      get '/search' do
-        number_of_results = params['number_of_results']
-        latitude = params['latitude'].to_f
-        longitude = params['longitude'].to_f
-        distance = params['distance']
-
-        RefugeRestroomsService.new.search_bathrooms(number_of_results, latitude, longitude, distance)
-      end
-    end
-  end
-  
+  root 'welcome#show'
 end
